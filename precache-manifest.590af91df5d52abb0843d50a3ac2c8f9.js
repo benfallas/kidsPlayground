b6c4af85f40868111f370fa5f7c983e472159e9a
Benito Sanchez <sanchezbenitog@gmail.com>
2022-09-10 20:30:30 -0700

    Updates

---
+++ precache-manifest.590af91df5d52abb0843d50a3ac2c8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4fe763ced6ed51e4a4f",
+    "url": "/benfallas/kidsPlayground/static/css/main.8da763ec.chunk.css"
+  },
+  {
+    "revision": "d4fe763ced6ed51e4a4f",
+    "url": "/benfallas/kidsPlayground/static/js/main.d4fe763c.chunk.js"
+  },
+  {
+    "revision": "a4b5b9e8f78e4cb2b3e7",
+    "url": "/benfallas/kidsPlayground/static/js/1.a4b5b9e8.chunk.js"
+  },
+  {
+    "revision": "588f177c00a2bb1e66d9",
+    "url": "/benfallas/kidsPlayground/static/js/runtime~main.588f177c.js"
+  },
+  {
+    "revision": "afab25e980c899cf503a181b41f742c6",
+    "url": "/benfallas/kidsPlayground/index.html"
+  }
+];
